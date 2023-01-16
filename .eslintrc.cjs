277d8a38faa0348ede8f72f83903c778c1047616
HiImConan <mindol0320@naver.com>
2023-01-16 14:46:52 +0900

    docs: 폴더 세팅

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	extends: ['plugin:@typescript-eslint/recommended', 'airbnb', 'airbnb-typescript', 'prettier'],
+	rules: {
+		'prefer-const': 'error',
+		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'no-console': 'warn',
+		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+	},
+	parserOptions: {
+		project: ['./tsconfig.json'], // Specify it only for TypeScript files
+	},
+}
